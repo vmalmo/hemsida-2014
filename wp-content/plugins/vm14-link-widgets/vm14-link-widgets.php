1ccad9059449530063b3efee374a3b3105eb844c
Richard Olsson <r@richardolsson.se>
2013-12-07 12:16:41 +0100

    Created first simple version of dynamic link widget.

--- wp-content/plugins/vm14-link-widgets/vm14-link-widgets.php
+++ wp-content/plugins/vm14-link-widgets/vm14-link-widgets.php
@@ -50,8 +50,100 @@ class VM14_Static_Link_Widget extends WP_Widget {
 	}
 }
 
+
+/**
+ * Dynamic link widget. Used to create a link to a single page which is
+ * chosen dynamically at render-time from a set of criteria.
+*/
+class VM14_Dynamic_Link_Widget extends WP_Widget {
+	function __construct() {
+		$widget_ops = array('description' => __('Use this widget to create a dynamic link to another page, chosen from a set a criteria.', 'vm14_link_widgets'));
+		parent::__construct('vm14_dynlink', __('Dynamic link', 'vm14_link_widgets'), $widget_ops);
+	}
+
+	function widget($args, $instance) {
+		extract($args);
+
+		$query = array(
+			'posts_per_page' => 1,
+			'category' => $instance['category_id']
+		);
+
+		switch ($instance['criterion']) {
+			case 'recent':
+				$query['orderby'] = 'post_date';
+				$query['order'] = 'DESC';
+				break;
+		}
+
+		$posts = get_posts($query);
+		if (sizeof($posts)==1) {
+			$post = $posts[0];
+
+			echo $before_widget;
+			printf('<a href="%s">%s</a>',
+				get_permalink($post->ID),
+				$post->post_title);
+			echo $after_widget;
+		}
+	}
+
+	function update($new_instance, $old_instance) {
+		$old_instance['criterion'] = $new_instance['criterion'];
+		$old_instance['category_id'] = $new_instance['category_id'];
+		return $old_instance;
+	}
+
+	function form($instance) {
+		$categories = get_categories(array('hide_empty' => false));
+		$criteria = array(
+			'recent' => __('Most recently published', 'vm14_link_widgets')
+		);
+		
+
+		// Criterion select box
+		printf('<label for="%s">%s:</label>',
+			$this->get_field_id('criterion'),
+			__('Criterion', 'vm14_link_widgets'));
+
+		printf('<select class="widefat" id="%s" name="%s">',
+			$this->get_field_id('criterion'),
+			$this->get_field_name('criterion'));
+
+		foreach ($criteria as $criterion => $name) {
+			printf('<option value="%s" %s>%s</option>',
+				$criterion,
+				selected($instance['criterion'], $criterion, false),
+				$name);
+		}
+
+		printf('</select>');
+
+
+		// Category select widget
+		printf('<label for="%s">%s:</label>',
+			$this->get_field_id('category_id'),
+			__('From category', 'vm14_link_widgets'));
+
+		printf('<select id="%s" name="%s">',
+			$this->get_field_id('category_id'),
+			$this->get_field_name('category_id'));
+
+		foreach ($categories as $category) {
+			printf('<option value="%s" %s>%s</option>',
+				intval($category->cat_ID),
+				selected($instance['category_id'], $category->cat_ID, false),
+				$category->name);
+		}
+
+		printf('</select>');
+	}
+}
+
+
 function vm14_link_widgets_init() {
 	register_widget('VM14_Static_Link_Widget');
+	register_widget('VM14_Dynamic_Link_Widget');
 }
 
 add_action('widgets_init', 'vm14_link_widgets_init');
