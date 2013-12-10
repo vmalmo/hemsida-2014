da90d52a3c1deca258456e7e6e928a1e8ae325c1
Jon <andersson.jon@gmail.com>
2013-12-10 13:54:52 +0100

    adding working group post type

--- wp-content/plugins/vm14-post-types/vm14-post-types.php
+++ wp-content/plugins/vm14-post-types/vm14-post-types.php
@@ -11,6 +11,7 @@
 require_once('vm14-post-type-base.php');
 require_once('vm14-contact-person.php');
 require_once('vm14-calendar-event.php');
+require_once('vm14-working-group.php');
 
 $vm14_contact_person_posttype = new VM14_Contact_Person_Posttype();
 $vm14_calendar_event_posttype = new VM14_Calendar_Event_Posttype();
---
+++ wp-content/plugins/vm14-post-types/vm14-working-group.php
@@ -0,0 +1,64 @@
+<?php
+class VM14_Working_Group_Posttype extends VM14_Posttype {
+  function __construct() {
+    VM14_Posttype::__construct( 'working_group', __('Working group'),
+      array( 'labels' => array(
+          'all_items' => __( 'All working groups', 'vm14' ), /* the all items menu item */
+        ), /* end of arrays */
+        'description' => __( 'Working groups in Vänsterpartiet Malmö', 'vm14' ), /* Custom Type Description */
+        'menu_position' => 8, /* this is what order you want it to appear in on the left hand side menu */ 
+        'rewrite'	=> array( 'slug' => 'working_group', 'with_front' => false ), /* you can specify its url slug */
+        'has_archive' => 'working_groups', /* you can rename the slug here */
+      )
+    );
+  }
+  function register_acf() {
+    register_field_group(array (
+      'id' => 'acf_normal_'. $this->posttype,
+      'title' => __($this->posttype_name).'s contact person',
+      'fields' => array (
+        array (
+          'key' => $this->posttype.'_contact_person_key',
+          'label' => '',
+          'name' => '',
+          'type' => 'relationship',
+          'return_format' => 'object',
+          'post_type' => array (
+            0 => 'contact_person',
+          ),
+          'taxonomy' => array (
+            0 => 'all',
+          ),
+          'filters' => array (
+            0 => 'search',
+          ),
+          'result_elements' => array (
+            0 => 'post_type',
+            1 => 'post_title',
+          ),
+          'max' => '',
+        ),
+      ),
+      'location' => array (
+        array (
+          array (
+            'param' => 'post_type',
+            'operator' => '==',
+            'value' => $this->posttype,
+            'order_no' => 0,
+            'group_no' => 0,
+          ),
+        ),
+      ),
+      'options' => array (
+        'position' => 'normal',
+        'hide_on_screen' => array (
+        ),
+      ),
+      'menu_order' => 0,
+    ));
+  }
+}
+// dunno if this works in this project, but nice to skip
+// bunch of lines that just says new
+$vm14_working_group_posttype = new VM14_Working_Group_Posttype();
