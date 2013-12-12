00b5c23a2a8126c04a8835fd978bca4fb8de8c3d
Jon <andersson.jon@gmail.com>
2013-12-12 19:31:53 +0100

    remove custom taxonomy

--- wp-content/plugins/vm14-post-types/vm14-post-types.php
+++ wp-content/plugins/vm14-post-types/vm14-post-types.php
@@ -15,18 +15,8 @@ require_once('types/vm14-calendar-event.php');
 require_once('types/vm14-working-group.php');
 require_once('types/vm14-section-entrance.php');
 
-require_once('vm14-taxonomy.php');
-
 $vm14_calendar_event_posttype = new VM14_Calendar_Event_Posttype();
 $vm14_contact_person_posttype = new VM14_Contact_Person_Posttype();
 $vm14_working_group_posttype = new VM14_Working_Group_Posttype();
 $vm14_section_entrance_posttype = new VM14_Section_Entrance_Posttype();
 
-$posttypes = array(
-  $vm14_working_group_posttype->posttype,
-  $vm14_contact_person_posttype->posttype,
-  $vm14_calendar_event_posttype->posttype,
-  $vm14_section_entrance_posttype->posttype
-);
-
-$vm14_taxonomy = new VM14_Taxonomy($posttypes);
