d94d8c2ee84f7c1d5826aaba776c370e4de63474
Richard Olsson <r@richardolsson.se>
2013-12-28 18:56:34 +0100

    Re-implemented section entrance post type using new framework.

--- wp-content/plugins/vm14-post-types/types/vm14-section-entrance.php
+++ wp-content/plugins/vm14-post-types/types/vm14-section-entrance.php
@@ -1,49 +1,8 @@
 <?php
-class VM14_Section_Entrance_Posttype extends VM14_Posttype {
-  function __construct() {
-    VM14_Posttype::__construct( 'section_entrance', __('Section Entrance'),
-      array( 'labels' => array(
-          'all_items' => __( 'All Section entrances', 'vm14' ), /* the all items menu item */
-        ), /* end of arrays */
-        'description' => __( 'Section entrances is the landingn page for on of the big menu points on the front page', 'vm14' ), /* Custom Type Description */
-        'menu_position' => 8, /* this is what order you want it to appear in on the left hand side menu */ 
-        'rewrite'	=> array( 'slug' => '/', 'with_front' => true ), /* you can specify its url slug */
-        'has_archive' => __('sections'), /* you can rename the slug here */
-      )
-    );
-  }
-
-  function register_acf() {
-    register_field_group(array (
-      'id' => 'acf_normal_'. $this->posttype,
-      'title' => __($this->posttype_name),
-      'fields' => array (
-        array (
-          'key' => $this->posttype.'_ingress_key',
-          'label' => __('Ingress'),
-          'name' => $this->posttype.'_ingress',
-          'type' => 'wysiwyg',
-          'media_upload' => false,
-        ),
-      ),
-      'location' => array (
-        array (
-          array (
-            'param' => 'post_type',
-            'operator' => '==',
-            'value' => $this->posttype,
-            'order_no' => 0,
-            'group_no' => 0,
-          ),
-        ),
-      ),
-      'options' => array (
-        'position' => 'normal',
-        'layout' => 'no_box',
-        'hide_on_screen' => array (
-        ),
-      ),
-      'menu_order' => 0,
-    ));
-  }
+class VM14_Section_Entrance_Post_Type extends VM14_Post_Type {
+    static $summary;
 }
+
+VM14_Section_Entrance_Post_Type::$summary = new VM14_Post_Type_Field(array(
+    'widget' => 'wysiwyg'
+));
--- wp-content/plugins/vm14-post-types/vm14-post-types.php
+++ wp-content/plugins/vm14-post-types/vm14-post-types.php
@@ -17,9 +17,9 @@ require_once('types/vm14-section-entrance.php');
 require_once('types/vm14-basic-page.php');
 
 $vm14_working_group_posttype = new VM14_Working_Group_Posttype();
-$vm14_section_entrance_posttype = new VM14_Section_Entrance_Posttype();
 
 add_action('init', function() {
+    VM14_Section_Entrance_Post_Type::register();
     VM14_Calendar_Event_Post_Type::register();
     VM14_Contact_Person_Post_Type::register();
     VM14_Page_Post_Type::register();
