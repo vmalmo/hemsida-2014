765a03a19e71f1b5406e7a98a0660cf546eff88d
Richard Olsson <r@richardolsson.se>
2014-01-04 23:18:49 +0100

    Added API to retrieve posts wrapped in custom post type classes.

--- wp-content/plugins/vm14-post-types/vm14-post-types.php
+++ wp-content/plugins/vm14-post-types/vm14-post-types.php
@@ -17,6 +17,30 @@ require_once('types/vm14-section-entrance.php');
 require_once('types/vm14-basic-page.php');
 require_once('types/vm14-post.php');
 
+function vm14_post($post) {
+    // TODO: Better way of figuring out the right post type?
+    switch ($post->post_type) {
+        default:
+            return new VM14_Post_Post_Type($post);
+    }
+}
+
+function vm14_get_post($id) {
+    $post = get_post($id);
+    return vm14_post($post);
+}
+
+function vm14_get_posts($args) {
+    $posts = get_posts($args);
+    for ($i=0; $i < count($posts); $i++) {
+        $posts[$i] = vm14_post($posts[$i]);
+
+        error_log($posts[$i]->summary);
+    }
+
+    return $posts;
+}
+
 add_action('init', function() {
     VM14_Section_Entrance_Post_Type::register();
     VM14_Calendar_Event_Post_Type::register();
