92e0c64090e3b1df0990e9f0add4396b097ec6d4
Jon <andersson.jon@gmail.com>
2014-01-04 21:39:58 +0100

    making top image a bit responsive

--- wp-content/themes/vansterpartietmalmo/template/start.php
+++ wp-content/themes/vansterpartietmalmo/template/start.php
@@ -5,16 +5,13 @@ Template Name: Start
 ?>
 
 <?php get_header(); ?>
-			<div class="videoplayer">
-
-			</div>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <?php $post = new VM14_Page_Post_Type(get_post()); ?>
+            <div class="videoplayer responsive-image" data-image-large="<?php echo $post->image_url('large') ?>"  style="background-image: url('<?php echo $post->image_url('medium') ?>');"> </div>
 			<div id="content">
-
-				
 				<div id="inner-content" class="wrap clearfix">
-						 <?php get_sidebar('sidebarHome'); ?>
+                    <?php get_sidebar('sidebarHome'); ?>
 				</div>
-
 			</div>
-
+    <?php endwhile; endif; ?>
 <?php get_footer(); ?>
