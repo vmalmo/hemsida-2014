5e7484d15fdc789fd9b35ed3064f71ce0387e71c
Richard Olsson <r@richardolsson.se>
2014-02-05 15:36:17 +0100

    Improved responsive image functionality on home page:
    - Now scales heightwise to fill part of screen
    - Better code structure

--- wp-content/themes/vansterpartietmalmo/library/js/vm14-responsive-images.js
+++ wp-content/themes/vansterpartietmalmo/library/js/vm14-responsive-images.js
@@ -1,34 +1,45 @@
 (function($) {
-    var ResponsiveImages = function() {
-        this.loadedIndex = 0;
-        this.viewport = $(window).width();
-        // add viewport map?
-        this.init();
-    }
-    ResponsiveImages.prototype.init = function() {
-        this.images = $('.responsive-image');
-        // right now ipad user get big images
-        if (this.viewport > 400) {
-            if (this.images.length > 0) {
-                this.load();
-            }
+    var initResponsiveImage = function(domElement) {
+        var heightRatio;
+
+        heightRatio = $(domElement).data('height-ratio');
+
+        if (heightRatio) {
+            var resize;
+
+            $(window).resize(resize = function() {
+                var h = Math.round(heightRatio * $(window).height());
+                $(domElement).css({
+                    'height': h + 'px'
+                });
+            });
+
+            resize();
         }
-    }
-    ResponsiveImages.prototype.load = function() {
-        if (this.loadedIndex < this.images.length) {
-            var image = new Image();
-            image.src = $(this.images[this.loadedIndex]).data('image-large');
+
+        // Load bigger image if big
+        if ($(window).width() > 400) {
+            var image;
+
+            image = new Image();
+            image.src = $(domElement).data('image-large');
             image.onload = function() {
-                $(this.images[this.loadedIndex]).css({
+                $(domElement).css({
                     'background-image': 'url('+image.src+')'
                 });
-                this.loadedIndex++;
-                this.load();
-            }.bind(this)
+            }
         }
-    }
+    };
 
     $(document).ready(function() {
-        var resImages = new ResponsiveImages();
+        $('.responsive-image').each(function() {
+            var heightRatio;
+
+            if ($(this).hasClass('home-poster'))
+                heightRatio = 0.85;
+            
+            initResponsiveImage(this, heightRatio);
+        });
+        //var resImages = new ResponsiveImages();
     });
 })(jQuery);
--- wp-content/themes/vansterpartietmalmo/template/start.php
+++ wp-content/themes/vansterpartietmalmo/template/start.php
@@ -8,7 +8,7 @@ Template Name: Start
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <?php $large_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' ); ?>
         <?php $medium_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' ); ?>
-        <div class="videoplayer responsive-image" data-image-large="<?php echo $large_image[0] ?>"  style="background-image: url('<?php echo $medium_image[0] ?>');"> </div>
+        <div class="videoplayer responsive-image" data-height-ratio="0.85" data-image-large="<?php echo $large_image[0] ?>"  style="background-image: url('<?php echo $medium_image[0] ?>');"> </div>
         <div id="content">
             <div id="inner-content" class="wrap clearfix">
                 <?php if ( is_active_sidebar( 'frontpage_blurbs' ) ) : ?>
