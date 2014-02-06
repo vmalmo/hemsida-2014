29ca0a4b4cbf0bc1cb478e5030a2321a7f0ea527
Richard Olsson <r@richardolsson.se>
2014-02-06 10:06:56 +0100

    Removed public help text for empty widget areas.

--- wp-content/themes/vansterpartietmalmo/sidebar.php
+++ wp-content/themes/vansterpartietmalmo/sidebar.php
@@ -4,14 +4,6 @@
 
 						<?php dynamic_sidebar( 'sidebar' ); ?>
 
-					<?php else : ?>
-
-						<?php // This content shows up if there are no widgets defined in the backend. ?>
-
-						<div class="alert alert-help">
-							<p><?php _e( 'Please activate some Widgets.', 'bonestheme' );  ?></p>
-						</div>
-
 					<?php endif; ?>
 
 				</div>
