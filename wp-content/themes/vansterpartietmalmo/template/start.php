b59cd8907bd85579f620a18d1cead356a451ff33
Richard Olsson <r@richardolsson.se>
2014-02-07 12:48:44 +0100

    Added intro and news placeholder on home page.

--- wp-content/themes/vansterpartietmalmo/template/start.php
+++ wp-content/themes/vansterpartietmalmo/template/start.php
@@ -8,9 +8,16 @@ Template Name: Start
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <?php $large_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' ); ?>
         <?php $medium_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' ); ?>
-        <div class="videoplayer responsive-image" data-height-ratio="0.85" data-image-large="<?php echo $large_image[0] ?>"  style="background-image: url('<?php echo $medium_image[0] ?>');"> </div>
+        <div class="videoplayer responsive-image" data-height-ratio="0.75" data-image-large="<?php echo $large_image[0] ?>"  style="background-image: url('<?php echo $medium_image[0] ?>');"> </div>
         <div id="content">
             <div id="inner-content" class="wrap clearfix">
+                <div id="home-intro">
+                    <h1><?php the_title();?></h1>
+                    <?php the_content();?>
+                </div>
+                <div id="home-news-marquee">
+                    Här rullar senaste nyheter.
+                </div>
                 <?php if ( is_active_sidebar( 'frontpage_blurbs' ) ) : ?>
                     <ul id="frontpage-blurbs">
                         <?php dynamic_sidebar( 'frontpage_blurbs' ); ?>
