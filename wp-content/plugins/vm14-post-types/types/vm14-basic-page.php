a88278700973550e5068bf638c7b8ff56cf5e758
Richard Olsson <r@richardolsson.se>
2014-02-09 09:47:56 +0100

    Enabled categories for pages as well.

--- wp-content/plugins/vm14-post-types/types/vm14-basic-page.php
+++ wp-content/plugins/vm14-post-types/types/vm14-basic-page.php
@@ -6,6 +6,8 @@ class VM14_Page_Post_Type extends VM14_Post_Type {
     static function register_type(&$meta) {
         // No need to register this type as it overrides the already
         // existing built-in page post type.
+        register_taxonomy_for_object_type('category', $meta['id']);
+        register_taxonomy_for_object_type('post_tag', $meta['id']);
     }
 }
 
