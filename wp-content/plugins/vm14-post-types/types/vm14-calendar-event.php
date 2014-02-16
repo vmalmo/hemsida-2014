77e3499227f5bfd2b42224418652a2aa396405ad
Richard Olsson <r@richardolsson.se>
2014-02-16 11:45:17 +0100

    Moved calendar event start/end date to custom field group.

--- wp-content/plugins/vm14-post-types/types/vm14-calendar-event.php
+++ wp-content/plugins/vm14-post-types/types/vm14-calendar-event.php
@@ -5,7 +5,17 @@ class VM14_Calendar_Event_Post_Type extends VM14_Post_Type {
     static $start_date;
     static $end_date;
     static $location;
+
+    static $meta_groups;
 }
+
+VM14_Calendar_Event_Post_Type::$meta_groups = array(
+    'date' => array(
+        'title' => __('Calendar event dates'),
+        'position' => 'side',
+        'layout' => 'box',
+    )
+);
 
 VM14_Calendar_Event_Post_Type::$summary = new VM14_Post_Type_Field(array(
     'widget' => 'wysiwyg',
@@ -13,10 +23,12 @@ VM14_Calendar_Event_Post_Type::$summary = new VM14_Post_Type_Field(array(
 
 VM14_Calendar_Event_Post_Type::$start_date = new VM14_Post_Type_Field(array(
     'widget' => 'date_picker',
+    'group' => 'date',
 ));
 
 VM14_Calendar_Event_Post_Type::$end_date = new VM14_Post_Type_Field(array(
     'widget' => 'date_picker',
+    'group' => 'date',
 ));
 
 VM14_Calendar_Event_Post_Type::$location = new VM14_Post_Type_Field(array(
