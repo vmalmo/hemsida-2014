d7eb2d94f325ce2e47e1e34f77d45e74e8df324f
Richard Olsson <r@richardolsson.se>
2014-02-16 09:44:05 +0100

    Removed some meta content in article listings.

--- wp-content/themes/vansterpartietmalmo/archive.php
+++ wp-content/themes/vansterpartietmalmo/archive.php
@@ -7,34 +7,24 @@
 						<div id="main" class="eightcol first clearfix" role="main">
 
 							<?php if (is_category()) { ?>
-								<h1 class="archive-title h2">
-									<span><?php _e( 'Posts Categorized:', 'bonestheme' ); ?></span> <?php single_cat_title(); ?>
-								</h1>
-
+								<h1 class="archive-title h2"><?php single_cat_title(); ?></h1>
 							<?php } elseif (is_tag()) { ?>
-								<h1 class="archive-title h2">
-									<span><?php _e( 'Posts Tagged:', 'bonestheme' ); ?></span> <?php single_tag_title(); ?>
-								</h1>
-
+								<h1 class="archive-title h2"><?php single_tag_title(); ?></h1>
 							<?php } elseif (is_author()) {
 								global $post;
 								$author_id = $post->post_author;
 							?>
 								<h1 class="archive-title h2">
-
 									<span><?php _e( 'Posts By:', 'bonestheme' ); ?></span> <?php the_author_meta('display_name', $author_id); ?>
-
 								</h1>
 							<?php } elseif (is_day()) { ?>
 								<h1 class="archive-title h2">
 									<span><?php _e( 'Daily Archives:', 'bonestheme' ); ?></span> <?php the_time('l, F j, Y'); ?>
 								</h1>
-
 							<?php } elseif (is_month()) { ?>
 									<h1 class="archive-title h2">
 										<span><?php _e( 'Monthly Archives:', 'bonestheme' ); ?></span> <?php the_time('F Y'); ?>
 									</h1>
-
 							<?php } elseif (is_year()) { ?>
 									<h1 class="archive-title h2">
 										<span><?php _e( 'Yearly Archives:', 'bonestheme' ); ?></span> <?php the_time('Y'); ?>
