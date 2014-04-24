8d4b866c135fa21ab5be3c807307a506221252a8
Jon Andersson <andersson.jon@gmail.com>
2014-04-24 17:24:38 +0200

    adding max size also to the front page cover image

--- wp-content/themes/vansterpartietmalmo/template/start.php
+++ wp-content/themes/vansterpartietmalmo/template/start.php
@@ -6,8 +6,8 @@ Template Name: Start
 
 <?php get_header(); ?>
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <?php $large_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'original' ); ?>
-        <?php $medium_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' ); ?>
+        <?php $large_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'vm14_full_width' ); ?>
+        <?php $medium_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'vm14_medium_width' ); ?>
         <div id="home-video" class="videoplayer responsive-image" data-youtube-id="<?php echo get_theme_mod('vm14_home_video');?>" data-height-ratio="0.75" data-image-large="<?php echo $large_image[0] ?>"  style="background-image: url('<?php echo $medium_image[0] ?>');">
         </div>
         <div id="content">
