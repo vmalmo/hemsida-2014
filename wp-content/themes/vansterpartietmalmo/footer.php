20abc1aa19632003bf02d8b88299ac4ed49a1676
Jon Andersson <andersson.jon@gmail.com>
2014-04-30 11:20:39 +0200

    adds google analytics id to General settings in admin.

--- wp-content/themes/vansterpartietmalmo/footer.php
+++ wp-content/themes/vansterpartietmalmo/footer.php
@@ -26,5 +26,20 @@
 		</div>
 		<?php // all js scripts are loaded in library/bones.php ?>
 		<?php wp_footer(); ?>
+
+    <?php if (get_option( 'vm14_google_analytics_id', '' )) { ?>
+          <script type="text/javascript">
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', '<?php echo get_option( 'vm14_google_analytics_id', '' ); ?>']);
+            _gaq.push(['_trackPageview']);
+            (function() {
+              var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+              ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+              var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+          </script>
+    <?php } ?>
+
+
 	</body>
 </html>
--- wp-content/themes/vansterpartietmalmo/functions.php
+++ wp-content/themes/vansterpartietmalmo/functions.php
@@ -212,6 +212,7 @@ function vm14_sub_menu($page) {
 
 
 function vm14_customize_register($wpc) {
+
     $wpc->add_section('vm14_section_footer', array(
         'title' => __( 'Footer content', 'vm14' ),
         'priority' => 30,
@@ -261,6 +262,25 @@ function vm14_customize_register($wpc) {
     )));
 }
 add_action('customize_register', 'vm14_customize_register');
+
+
+/** ADDS google analytics to general settings in admin **/
+$new_general_setting = new new_general_setting();
+
+class new_general_setting {
+  function new_general_setting( ) {
+    add_filter( 'admin_init' , array( &$this , 'register_fields' ) );
+  }
+  function register_fields() {
+    register_setting( 'general', 'vm14_google_analytics_id', 'esc_attr' );
+    add_settings_field('vm14_google_analytics_id', '<label for="vm14_google_analytics_id">'.__('Google Analytics ID' , 'vm14' ).'</label>' , array(&$this, 'fields_html') , 'general' );
+  }
+  function fields_html() {
+    $value = get_option( 'vm14_google_analytics_id', '' );
+    echo '<input type="text" id="vm14_google_analytics_id" name="vm14_google_analytics_id" value="' . $value . '" />';
+  }
+}
+
 
 function vm14_post_header($type = 'post'){
   $classes = array('page-header', 'clearfix');
