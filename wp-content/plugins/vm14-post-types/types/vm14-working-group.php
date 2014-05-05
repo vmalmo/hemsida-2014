f271a4a6742c135c8b19d1689792bda3d46b3c54
Richard Olsson <r@richardolsson.se>
2014-05-05 08:53:19 +0200

    Added events() to working group post type
    Makes a reverse query to retrieve all events which have the current working group
    assigned as the arranger.

--- wp-content/plugins/vm14-post-types/types/vm14-working-group.php
+++ wp-content/plugins/vm14-post-types/types/vm14-working-group.php
@@ -3,6 +3,29 @@ class VM14_Working_Group_Post_Type extends VM14_Post_Type {
     static $summary;
     static $contact_person;
     static $meta_slug = 'arbetsgrupp';//TODO: add localization
+
+    public function events($limit = 3) {
+        return vm14_get_posts(array(
+            'post_type' => 'calendar_event',
+            'posts_per_page' => $limit,
+            'orderby' => 'meta_value',
+            'meta_key' => 'calendar_event_start_date',
+            'meta_query' => array(
+                array(
+                  'key' => 'calendar_event_end_date',
+                  'value' => date('Ymd', strtotime($today. ' - 1 days')),
+                  'type' => 'numeric',
+                  'compare' => '>='
+                ),
+                array(
+                    'key' => 'calendar_event_working_group',
+                    'value' => '"'.$this->id.'"',
+                    'compare' => 'LIKE'
+                )
+            ),
+            'order' => 'ASC'
+        ));
+    }
 }
 
 VM14_Working_Group_Post_Type::$summary = new VM14_Post_Type_Field(array(
