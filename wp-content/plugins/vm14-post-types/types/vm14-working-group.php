95992e448845a466f89acb9dc7cc26e67daf4933
Richard Olsson <r@richardolsson.se>
2014-05-05 09:41:56 +0200

    Renamed contact_person field "contact_persons" i working group type.

--- wp-content/plugins/vm14-post-types/types/vm14-working-group.php
+++ wp-content/plugins/vm14-post-types/types/vm14-working-group.php
@@ -1,7 +1,7 @@
 <?php
 class VM14_Working_Group_Post_Type extends VM14_Post_Type {
     static $summary;
-    static $contact_person;
+    static $contact_persons;
     static $meta_slug = 'arbetsgrupp';//TODO: add localization
 
     public function events($limit = 3) {
@@ -32,5 +32,5 @@ VM14_Working_Group_Post_Type::$summary = new VM14_Post_Type_Field(array(
     'widget' => 'wysiwyg'
 ));
 
-VM14_Working_Group_Post_Type::$contact_person = new VM14_Post_Type_Relationship('contact_person');
+VM14_Working_Group_Post_Type::$contact_persons = new VM14_Post_Type_Relationship('contact_person');
 
