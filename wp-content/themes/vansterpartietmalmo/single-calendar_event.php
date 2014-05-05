e73190f295b8002a1d14fa6ae29c193805448dcf
Jon Andersson <andersson.jon@gmail.com>
2014-05-05 15:06:13 +0200

    remove top/cover image

--- wp-content/themes/vansterpartietmalmo/single-calendar_event.php
+++ wp-content/themes/vansterpartietmalmo/single-calendar_event.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <?php vm14_post_header('single'); ?>
         <?php echo vm14_sub_menu($post->ID); ?>
         <?php $p = vm14_get_post($post->ID);?>
         <div id="inner-content" class="wrap clearfix">
             <?php vm14_breadcrumbs($post->ID); ?>
             <div id="main" class="eightcol first clearfix" role="main">
                 <article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+                    <h1 class="page-title"><?php the_title(); ?></h1>
                     <section class="entry-content clearfix" itemprop="articleBody">
                         <?php the_content(); ?>
                     </section>
@@ -72,6 +72,16 @@ Till eventet på Facebook</a>
                             <?php echo $wg->preview_html();?>
                         </li>
                     <?php endif;?>
+                    <?php if ($p->show_share_buttons) : ?>
+                      <li>
+                          <h5>Dela</h5>
+                          <div class="social-share-buttons">
+                              <a href="https://twitter.com/share" class="twitter-share-button" data-lang="sv">Tweeta</a>
+                              <div class="fb-like" data-layout="button" data-action="like" data-show-faces="false" data-share="true"></div>
+                          </div>
+                      </li>
+                    <?php endif; ?>
+
 
                   </ul>
 
