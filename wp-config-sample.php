769bb0af71cab522c50219d19c6b1b3e135b5149
Richard Olsson <r@richardolsson.se>
2014-06-05 17:52:00 +0200

    Updated Wordpress to 3.9.1 (Swedish)

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -1,90 +1,90 @@
 <?php
 /**
- * The base configurations of the WordPress.
+ * Baskonfiguration för WordPress.
  *
- * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, WordPress Language, and ABSPATH. You can find more information
- * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. You can get the MySQL settings from your web host.
+ * Denna fil innehåller följande konfigurationer: Inställningar för MySQL,
+ * Tabellprefix, Säkerhetsnycklar, WordPress-språk, och ABSPATH.
+ * Mer information på {@link http://codex.wordpress.org/Editing_wp-config.php 
+ * Editing wp-config.php}. MySQL-uppgifter får du från ditt webbhotell.
  *
- * This file is used by the wp-config.php creation script during the
- * installation. You don't have to use the web site, you can just copy this file
- * to "wp-config.php" and fill in the values.
+ * Denna fil används av wp-config.php-genereringsskript under installationen.
+ * Du behöver inte använda webbplatsen, du kan kopiera denna fil direkt till
+ * "wp-config.php" och fylla i värdena.
  *
  * @package WordPress
  */
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+// ** MySQL-inställningar - MySQL-uppgifter får du från ditt webbhotell ** //
+/** Namnet på databasen du vill använda för WordPress */
+define('DB_NAME', 'Ange databasnamn här');
 
-/** MySQL database username */
-define('DB_USER', 'username_here');
+/** MySQL-databasens användarnamn */
+define('DB_USER', 'Ange databasanvändare här');
 
-/** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+/** MySQL-databasens lösenord */
+define('DB_PASSWORD', 'Ange databaslösenord här');
 
-/** MySQL hostname */
+/** MySQL-server */
 define('DB_HOST', 'localhost');
 
-/** Database Charset to use in creating database tables. */
+/** Teckenkodning för tabellerna i databasen. */
 define('DB_CHARSET', 'utf8');
 
-/** The Database Collate type. Don't change this if in doubt. */
+/** Kollationeringstyp för databasen. Ändra inte om du är osäker. */
 define('DB_COLLATE', '');
 
 /**#@+
- * Authentication Unique Keys and Salts.
+ * Unika autentiseringsnycklar och salter.
  *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ * Ändra dessa till unika fraser!
+ * Du kan generera nycklar med {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * Du kan när som helst ändra dessa nycklar för att göra aktiva cookies obrukbara, vilket tvingar alla användare att logga in på nytt.
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         'Ange en unik fras här');
+define('SECURE_AUTH_KEY',  'Ange en unik fras här');
+define('LOGGED_IN_KEY',    'Ange en unik fras här');
+define('NONCE_KEY',        'Ange en unik fras här');
+define('AUTH_SALT',        'Ange en unik fras här');
+define('SECURE_AUTH_SALT', 'Ange en unik fras här');
+define('LOGGED_IN_SALT',   'Ange en unik fras här');
+define('NONCE_SALT',       'Ange en unik fras här');
 
 /**#@-*/
 
 /**
- * WordPress Database Table prefix.
+ * Tabellprefix för WordPress Databasen.
  *
- * You can have multiple installations in one database if you give each a unique
- * prefix. Only numbers, letters, and underscores please!
+ * Du kan ha flera installationer i samma databas om du ger varje installation ett unikt
+ * prefix. Endast siffror, bokstäver och understreck!
  */
 $table_prefix  = 'wp_';
 
 /**
- * WordPress Localized Language, defaults to English.
+ * WordPress-språk, förinställt för svenska.
  *
- * Change this to localize WordPress. A corresponding MO file for the chosen
- * language must be installed to wp-content/languages. For example, install
- * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
- * language support.
+ * Du kan ändra detta för att ändra språk för WordPress.  En motsvarande .mo-fil
+ * för det valda språket måste finnas i wp-content/languages. Exempel, lägg till
+ * sv_SE.mo i wp-content/languages och ange WPLANG till 'sv_SE' för att få sidan
+ * på svenska.
  */
-define('WPLANG', '');
+define('WPLANG', 'sv_SE');
 
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- */
+/** 
+ * För utvecklare: WordPress felsökningsläge. 
+ * 
+ * Ändra detta till true för att aktivera meddelanden under utveckling. 
+ * Det är rekommderat att man som tilläggsskapare och temaskapare använder WP_DEBUG 
+ * i sin utvecklingsmiljö. 
+ */ 
 define('WP_DEBUG', false);
 
-/* That's all, stop editing! Happy blogging. */
+/* Det var allt, sluta redigera här! Blogga på. */
 
-/** Absolute path to the WordPress directory. */
+/** Absoluta sökväg till WordPress-katalogen. */
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
-/** Sets up WordPress vars and included files. */
+/** Anger WordPress-värden och inkluderade filer. */
 require_once(ABSPATH . 'wp-settings.php');
