98185f8328d0f54d248af054b545b9ecb828ef66
Richard Olsson <r@richardolsson.se>
2014-07-31 15:53:20 +0200

    Added titles and contact info to contact person list.

--- wp-content/plugins/vm14-post-types/types/vm14-contact-person.php
+++ wp-content/plugins/vm14-post-types/types/vm14-contact-person.php
@@ -12,11 +12,17 @@ class VM14_Contact_Person_Post_Type extends VM14_Post_Type {
     static $meta_slug = 'kontaktperson'; //TODO: add localization
 
     function preview_html() {
-        $excerpt = $this->get_excerpt(300);
         $html  = sprintf('<a href="%s">', get_permalink($this->id));
         $html .= sprintf('<h4>%s</h4>', $this->first_name . ' ' .$this->last_name);
         $html .= get_the_post_thumbnail($this->id);
-        $html .= sprintf('<p>%s</p>', $excerpt);
+        $html .= sprintf('<span class="titles">%s</span>', $this->titles);
+
+        $contact = array();
+        if ($this->email) array_push($contact, $this->email);
+        if ($this->phone) array_push($contact, $this->phone);
+        $html .= sprintf('<small class="contact">%s</small>', implode(', ', $contact));
+
+        $html .= sprintf('<p>%s</p>', $this->get_excerpt(100));
         $html .= '</a>';
 
         return $html;
